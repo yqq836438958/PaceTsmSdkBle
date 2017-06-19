6fddf99d499d06fb61fc366803dac93c078c7c2e
qingqingyuan <qingqingyuan@pacewear.cn>
2017-06-19 17:05:37 +0800

    set BleAccessPoint parcelable

--- src/com/pace/data/BleAccessPoint.java
+++ src/com/pace/data/BleAccessPoint.java
@@ -1,7 +1,10 @@
 
 package com.pace.data;
 
-public class BleAccessPoint {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class BleAccessPoint implements Parcelable {
     private String serviceUUID;
     private String writeCharater;
     private String indicatCharater;
@@ -12,6 +15,13 @@ public class BleAccessPoint {
         writeCharater = writeChar;
         indicatCharater = indicateChar;
         targetMacAddr = targetMac;
+    }
+
+    public BleAccessPoint(Parcel source) {
+        serviceUUID = source.readString();
+        writeCharater = source.readString();
+        indicatCharater = source.readString();
+        targetMacAddr = source.readString();
     }
 
     public String getServiceUUID() {
@@ -29,4 +39,30 @@ public class BleAccessPoint {
     public String getTargeMacAddr() {
         return targetMacAddr;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(serviceUUID);
+        dest.writeString(writeCharater);
+        dest.writeString(indicatCharater);
+        dest.writeString(targetMacAddr);
+    }
+
+    public static final Creator<BleAccessPoint> CREATOR = new Creator<BleAccessPoint>() {
+
+        @Override
+        public BleAccessPoint createFromParcel(Parcel source) {
+            return new BleAccessPoint(source);
+        }
+
+        @Override
+        public BleAccessPoint[] newArray(int size) {
+            return new BleAccessPoint[size];
+        }
+    };
 }
